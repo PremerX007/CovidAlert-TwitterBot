109664235a1415d1d02b8395e2eaa5cea6cbe5f4
Bannawat Vongpooton <bannawat_55120@outlook.com>
2022-08-21 12:28:18 +0700

    Add Folder .shared (api key package)

---
+++ v0.0.3/shared/api.py
@@ -0,0 +1,9 @@
+"""
+Your can get Twitter API Key from https://developer.twitter.com/en/products/twitter-api
+"""
+API_KEY = "***** API_KEY *****"
+API_SECRET_KEY = "***** API_SECRET_KEY *****"
+ACCESS_TOKEN = "***** ACCESS_TOKEN *****"
+SECRET_ACCESS_TOKEN = "***** SECRET_ACCESS_TOKEN *****"
+TWITTER_ID = '***** Your TWITTER_ID *****'
+LINE_TOKEN = "***** Your LINE_TOKEN *****" # Get LINE token from https://notify-bot.line.me
