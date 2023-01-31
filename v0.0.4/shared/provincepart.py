209d5e3142ff1ed9efd47a40f5ca2c729bcf667a
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-01-31 14:17:25 +0700

    change the method for sorting data.

--- v0.0.4/shared/provincepart.py
+++ v0.0.4/shared/provincepart.py
@@ -1,84 +1,12 @@
-north_index = ['\u0e40\u0e0a\u0e35\u0e22\u0e07\u0e23\u0e32\u0e22',
-            '\u0e40\u0e0a\u0e35\u0e22\u0e07\u0e43\u0e2b\u0e21\u0e48',
-            '\u0e25\u0e33\u0e1b\u0e32\u0e07',
-            '\u0e25\u0e33\u0e1e\u0e39\u0e19',
-            '\u0e41\u0e21\u0e48\u0e2e\u0e48\u0e2d\u0e07\u0e2a\u0e2d\u0e19',
-            '\u0e1e\u0e30\u0e40\u0e22\u0e32',
-            '\u0e19\u0e48\u0e32\u0e19',
-            '\u0e41\u0e1e\u0e23\u0e48',
-            '\u0e2d\u0e38\u0e15\u0e23\u0e14\u0e34\u0e15\u0e16\u0e4c',
-            '\u0e15\u0e32\u0e01']
+import collections
 
-northeast_index = ['\u0e2d\u0e33\u0e19\u0e32\u0e08\u0e40\u0e08\u0e23\u0e34\u0e0d',
-            '\u0e1a\u0e36\u0e07\u0e01\u0e32\u0e2c',
-            '\u0e1a\u0e38\u0e23\u0e35\u0e23\u0e31\u0e21\u0e22\u0e4c',
-            '\u0e0a\u0e31\u0e22\u0e20\u0e39\u0e21\u0e34',
-            '\u0e01\u0e32\u0e2c\u0e2a\u0e34\u0e19\u0e18\u0e38\u0e4c',
-            '\u0e02\u0e2d\u0e19\u0e41\u0e01\u0e48\u0e19',
-            '\u0e40\u0e25\u0e22',
-            '\u0e21\u0e2b\u0e32\u0e2a\u0e32\u0e23\u0e04\u0e32\u0e21',
-            '\u0e21\u0e38\u0e01\u0e14\u0e32\u0e2b\u0e32\u0e23',
-            '\u0e19\u0e04\u0e23\u0e1e\u0e19\u0e21',
-            '\u0e19\u0e04\u0e23\u0e23\u0e32\u0e0a\u0e2a\u0e35\u0e21\u0e32',
-            '\u0e2b\u0e19\u0e2d\u0e07\u0e1a\u0e31\u0e27\u0e25\u0e33\u0e20\u0e39',
-            '\u0e2b\u0e19\u0e2d\u0e07\u0e04\u0e32\u0e22',
-            '\u0e23\u0e49\u0e2d\u0e22\u0e40\u0e2d\u0e47\u0e14',
-            '\u0e2a\u0e01\u0e25\u0e19\u0e04\u0e23',
-            '\u0e28\u0e23\u0e35\u0e2a\u0e30\u0e40\u0e01\u0e29',
-            '\u0e2a\u0e38\u0e23\u0e34\u0e19\u0e17\u0e23\u0e4c',
-            '\u0e2d\u0e38\u0e1a\u0e25\u0e23\u0e32\u0e0a\u0e18\u0e32\u0e19\u0e35',
-            '\u0e2d\u0e38\u0e14\u0e23\u0e18\u0e32\u0e19\u0e35',
-            '\u0e22\u0e42\u0e2a\u0e18\u0e23']
-
-central_index = ['\u0e01\u0e23\u0e38\u0e07\u0e40\u0e17\u0e1e\u0e21\u0e2b\u0e32\u0e19\u0e04\u0e23',
-            '\u0e1e\u0e23\u0e30\u0e19\u0e04\u0e23\u0e28\u0e23\u0e35\u0e2d\u0e22\u0e38\u0e18\u0e22\u0e32',
-            '\u0e1b\u0e17\u0e38\u0e21\u0e18\u0e32\u0e19\u0e35',
-            '\u0e19\u0e19\u0e17\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e19\u0e04\u0e23\u0e1b\u0e10\u0e21',
-            '\u0e2a\u0e21\u0e38\u0e17\u0e23\u0e1b\u0e23\u0e32\u0e01\u0e32\u0e23',
-            '\u0e2d\u0e48\u0e32\u0e07\u0e17\u0e2d\u0e07',
-            '\u0e0a\u0e31\u0e22\u0e19\u0e32\u0e17',
-            '\u0e25\u0e1e\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e2a\u0e21\u0e38\u0e17\u0e23\u0e2a\u0e07\u0e04\u0e23\u0e32\u0e21',
-            '\u0e2a\u0e21\u0e38\u0e17\u0e23\u0e2a\u0e32\u0e04\u0e23',
-            '\u0e2a\u0e23\u0e30\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e2a\u0e34\u0e07\u0e2b\u0e4c\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e2a\u0e38\u0e1e\u0e23\u0e23\u0e13\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e19\u0e04\u0e23\u0e19\u0e32\u0e22\u0e01',
-            '\u0e01\u0e32\u0e0d\u0e08\u0e19\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e1b\u0e23\u0e30\u0e08\u0e27\u0e1a\u0e04\u0e35\u0e23\u0e35\u0e02\u0e31\u0e19\u0e18\u0e4c',
-            '\u0e23\u0e32\u0e0a\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e40\u0e1e\u0e0a\u0e23\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e2a\u0e38\u0e42\u0e02\u0e17\u0e31\u0e22',
-            '\u0e1e\u0e34\u0e08\u0e34\u0e15\u0e23',
-            '\u0e1e\u0e34\u0e29\u0e13\u0e38\u0e42\u0e25\u0e01',
-            '\u0e01\u0e33\u0e41\u0e1e\u0e07\u0e40\u0e1e\u0e0a\u0e23',
-            '\u0e40\u0e1e\u0e0a\u0e23\u0e1a\u0e39\u0e23\u0e13\u0e4c',
-            '\u0e19\u0e04\u0e23\u0e2a\u0e27\u0e23\u0e23\u0e04\u0e4c',
-            '\u0e2d\u0e38\u0e17\u0e31\u0e22\u0e18\u0e32\u0e19\u0e35']
-
-east_index = ['\u0e08\u0e31\u0e19\u0e17\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e09\u0e30\u0e40\u0e0a\u0e34\u0e07\u0e40\u0e17\u0e23\u0e32',
-            '\u0e0a\u0e25\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e15\u0e23\u0e32\u0e14',
-            '\u0e1b\u0e23\u0e32\u0e08\u0e35\u0e19\u0e1a\u0e38\u0e23\u0e35',
-            '\u0e23\u0e30\u0e22\u0e2d\u0e07',
-            '\u0e2a\u0e23\u0e30\u0e41\u0e01\u0e49\u0e27']
-
-south_index = ['\u0e01\u0e23\u0e30\u0e1a\u0e35\u0e48',
-            '\u0e0a\u0e38\u0e21\u0e1e\u0e23',
-            '\u0e15\u0e23\u0e31\u0e07',
-            '\u0e19\u0e04\u0e23\u0e28\u0e23\u0e35\u0e18\u0e23\u0e23\u0e21\u0e23\u0e32\u0e0a',
-            '\u0e19\u0e23\u0e32\u0e18\u0e34\u0e27\u0e32\u0e2a',
-            '\u0e1b\u0e31\u0e15\u0e15\u0e32\u0e19\u0e35',
-            '\u0e1e\u0e31\u0e07\u0e07\u0e32',
-            '\u0e1e\u0e31\u0e17\u0e25\u0e38\u0e07',
-            '\u0e20\u0e39\u0e40\u0e01\u0e47\u0e15',
-            '\u0e22\u0e30\u0e25\u0e32',
-            '\u0e23\u0e30\u0e19\u0e2d\u0e07',
-            '\u0e2a\u0e07\u0e02\u0e25\u0e32',
-            '\u0e2a\u0e15\u0e39\u0e25',
-            '\u0e2a\u0e38\u0e23\u0e32\u0e29\u0e0e\u0e23\u0e4c\u0e18\u0e32\u0e19\u0e35']
+country = {
+    'north'     : ['เชียงราย','เชียงใหม่','ลำปาง','ลำพูน','แม่ฮ่องสอน','พะเยา','น่าน','แพร่','อุตรดิตถ์','ตาก'],
+    'northeast' : ['อำนาจเจริญ','บึงกาฬ','บุรีรัมย์','ชัยภูมิ','กาฬสินธุ์','ขอนแก่น','เลย','มหาสารคาม','มุกดาหาร','นครพนม','นครราชสีมา','หนองบัวลำภู','หนองคาย','ร้อยเอ็ด','สกลนคร','ศรีสะเกษ','สุรินทร์','อุบลราชธานี','อุดรธานี','ยโสธร'],
+    'central'   : ['กรุงเทพมหานคร','พระนครศรีอยุธยา','ปทุมธานี','นนทบุรี','นครปฐม','สมุทรปราการ','อ่างทอง','ชัยนาท','ลพบุรี','สมุทรสงคราม','สมุทรสาคร','สระบุรี','สิงห์บุรี','สุพรรณบุรี','นครนายก','กาญจนบุรี','ประจวบคีรีขันธ์','ราชบุรี','เพชรบุรี','สุโขทัย','พิจิตร','พิษณุโลก','กำแพงเพชร','เพชรบูรณ์','นครสวรรค์','อุทัยธานี'],
+    'east'      : ['จันทบุรี','ฉะเชิงเทรา','ชลบุรี','ตราด','ปราจีนบุรี','ระยอง','สระแก้ว'],
+    'south'     : ['กระบี่','ชุมพร','ตรัง','นครศรีธรรมราช','นราธิวาส','ปัตตานี','พังงา','พัทลุง','ภูเก็ต','ยะลา','ระนอง','สงขลา','สตูล','สุราษฎร์ธานี']
+}
             
 alldict = {}
 north = []
@@ -87,49 +15,34 @@ central = []
 east = []
 south = []
 
+def checker(info):
+    for zone in country:
+        for province in country[zone]:
+            if info['province'] == province:
+                return zone
+                
 def province_part(data):
-    
-    for pv in data:
-        for a in north_index:
-            if pv['province'] == a:
-                north.append(data.index(pv))
-                north_index.remove(a)
-                break
-        
-        for b in northeast_index:
-            if pv['province'] == b:
-                northeast.append(data.index(pv))
-                northeast_index.remove(b)
-                break
-        
-        for d in central_index:
-            if pv['province'] == d:
-                central.append(data.index(pv))
-                central_index.remove(d)
-                break
-        
-        for e in east_index:
-            if pv['province'] == e:
-                east.append(data.index(pv))
-                east_index.remove(e)
-                break
-        
-        for f in south_index:
-            if pv['province'] == f:
-                south.append(data.index(pv))
-                south_index.remove(f)
-                break
+    for info in data:
+        temp = checker(info)
+        if temp == 'north': north.append(data.index(info))
+        elif temp == 'northeast': northeast.append(data.index(info))
+        elif temp == 'central': central.append(data.index(info))
+        elif temp == 'east': east.append(data.index(info))
+        elif temp == 'south': south.append(data.index(info))
+        else: pass
+
+    ' Re-checking when does not match. [Testing..] '
+    # if collections.Counter([data[x]['province'] for x in north]) != collections.Counter(country['north']): return 1
 
     alldict['north'] = north
     alldict['northeast'] = northeast
     alldict['central'] = central
     alldict['east'] = east
     alldict['south'] = south
-
-    def sort(k):
+    
+    def sorten(k):
         return data[k]['new_case']
 
-    for i in alldict:
-        alldict[i].sort(reverse=True,key=sort)
+    for i in alldict: alldict[i].sort(reverse=True,key=sorten)
 
     return alldict
